18902291decc3a80d085cbc27cc6ea7e8f446cd3
Roh Jimin <94786383+Rogan-J@users.noreply.github.com>
2022-08-02 17:01:52 +0900

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '9c4c004125f4491d9aa63440c7c2cfb2',
+  rootNotionPageId: '31ba3e5690904ad69ba13c1ed782ab34',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Jimin',
+  name: 'NonBat',
   domain: 'nonbat.org',
-  author: 'JIMIN',
+  author: 'Nonbat',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
